e320f998f1d613675d88c260eb1436c590018bd3
jdm365 <jdm365@georgetown.edu>
2023-04-05 13:11:11 -0400

    unsupervised hashing. Creating embeddings for faiss by getting sim w/  randomly selected strings seems to work well and is performant.

--- minhashing/min_hash.py
+++ minhashing/min_hash.py
@@ -1,8 +1,16 @@
 import pandas as pd
+import numpy as np
 from hashlib import sha1
+import random
 from datasketch import MinHash, MinHashLSH
 from tqdm import tqdm
 from time import perf_counter
+from jarowinkler import jarowinkler_similarity
+from rapidfuzz.process import cdist
+import faiss
+
+import sys
+sys.path.append('../')
 from utils.address_handler import get_addresses
 from utils.fuzzify import Fuzzifier
 
@@ -52,13 +60,68 @@ def shingle(string, n_grams=4):
     return shingles
 
 
+def get_embeddings_simvec(addresses, dim=128, metric=jarowinkler_similarity):
+    random_addresses = np.random.choice(list(addresses), dim)
+
+    embeddings = cdist(addresses, random_addresses, scorer=metric)
+    return embeddings
+
+def dedupe_faiss(addresses, cutoff=0.1, k=10):
+    """
+    Remove duplicate addresses
+    """
+    embeddings = get_embeddings_simvec(addresses, dim=128, metric=jarowinkler_similarity)
+
+    ## Create index
+    dim = embeddings.shape[1]
+    quantizer = faiss.IndexFlatL2(dim)
+
+    n_centroids = 64
+    n_bits = 8
+    n_vornoi_cells = 1024
+
+    index = faiss.IndexIVFPQ(quantizer, dim, n_vornoi_cells, n_centroids, n_bits)
+    index.nprobe = int(np.sqrt(n_vornoi_cells))
+    index.train(embeddings)
+    index.add(embeddings)
+
+    ## Get k nearest neighbours
+    distances, indices = index.search(embeddings, k)
+
+    match_df = pd.DataFrame({
+        'orig_idxs': np.arange(k * len(addresses)) // k,
+        'match_idxs': indices.flatten(),
+        'distance': distances.flatten()
+    })
+    ## Get unique edges and non-self edges
+    match_df = match_df[match_df['orig_idxs'] != match_df['match_idxs']]
+    match_df = match_df[match_df['orig_idxs'] < match_df['match_idxs']]
+
+    match_df['orig_address']  = addresses[match_df['orig_idxs']]
+    match_df['match_address'] = addresses[match_df['match_idxs']]
+
+    match_df = match_df[match_df['distance'] < cutoff]
+    return match_df
+
 
 
 
 if __name__ == '__main__':
-    data = pd.read_feather('data/extracted_addresses.feather')
+    data = pd.read_feather('../data/extracted_addresses.feather')
 
-    addresses = data['address'].iloc[:10000]
+    data = data[data['address'] != '']
+    data = data[data['address'].isnull() == False]
+
+    SUBSET_SIZE = 500_000
+    random.seed(42)
+
+    data['address'] = data['address'].apply(lambda x: x.strip())
+    data = data[data['address'].apply(lambda x: len(x) > 1)]
+    addresses = np.random.choice(data['address'], SUBSET_SIZE, replace=False)
+    ## Remove nulls
+    addresses = addresses[addresses != '']
+
+    print('Number of addresses: {}'.format(len(addresses)))
 
     fuzzifier = Fuzzifier()
 
@@ -67,12 +130,18 @@ if __name__ == '__main__':
 
 
     start = perf_counter()
-    match_idxs = get_similar_docs(addresses, query_address, num_perm=8, threshold=0.5)
+    #match_idxs = get_similar_docs(addresses, query_address, num_perm=8, threshold=0.5)
+    match_df = dedupe_faiss(addresses)
+
+    DISPLAY_COLS = ['orig_address', 'match_address', 'distance']
+    print(match_df[DISPLAY_COLS])
     end = perf_counter()
 
+    """
     print(f'\nQuery address: {query_address}')
     print('Matched addresses:')
     print(data.iloc[match_idxs])
 
     print(f'Actual jaccard similarity: {get_jaccard(query_address, addresses[500])}')
     print('Time taken: {} seconds'.format(end - start))
+    """
