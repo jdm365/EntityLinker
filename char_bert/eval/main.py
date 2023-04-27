e4974abb92c1e370b4a883ac454393773237f305
jdm365 <jdm365@georgetown.edu>
2023-04-27 11:43:53 -0400

    Added byt5 support. Will evaluate later.

--- char_bert/eval/main.py
+++ char_bert/eval/main.py
@@ -6,6 +6,7 @@ import logging
 
 from char_bert.src.main import *
 from char_bert.src.utils import *
+from char_bert.src.test_model import HuggingFaceByt5Wrapper
 
 
 def test_dedup(dedup_func, data, dedup_col, **kwargs):
@@ -49,6 +50,51 @@ def test_sim_search_faiss(data, index_col, search_col, k=10):
 
     index_embeddings  = get_embeddings(data[index_col].to_list())
     search_embeddings = get_embeddings(data[search_col].to_list())
+
+    ## Create index
+    index = create_faiss_index(index_embeddings)
+
+    ## Search index
+    distances, indices = index.search(search_embeddings, k=k)
+
+    match_df = pd.DataFrame({
+        'orig_idxs': np.arange(k * len(data)) // k,
+        'match_idxs': indices.flatten(),
+        'distance': distances.flatten()
+    })
+
+    match_df['orig_label']  = np.array(data['label'].values)[match_df['orig_idxs'].values.astype(int)]
+    match_df['match_label'] = np.array(data['label'].values)[match_df['match_idxs'].values.astype(int)]
+    match_df['is_match']    = (match_df['orig_label'] == match_df['match_label']).astype(int)
+
+    print(f'Accuracy:  {np.sum(match_df["is_match"]) / len(match_df)}')
+    print(f'Recall:    {np.sum(match_df["is_match"]) / len(data)}')
+
+    print('Time taken: {} seconds'.format(perf_counter() - start))
+
+
+def test_sim_search_faiss_byt5(data, index_col, search_col, k=10):
+    """
+    Test similarity search function
+    @param sim_search_func: similarity search function
+    @param data: dataframe containing data to search
+    @param search_col: column to search
+    @param kwargs: keyword arguments to pass to similarity search function
+    return: None
+    """
+    start = perf_counter()
+    model = HuggingFaceByt5Wrapper()
+
+    index_embeddings = []
+    search_embeddings = []
+    for idx, batch in enumerate(tqdm(data[index_col].to_list())):
+        index_embeddings.append(model.get_embeddings(batch))
+
+    for idx, batch in enumerate(tqdm(data[search_col].to_list())):
+        search_embeddings.append(model.get_embeddings(batch))
+
+    index_embeddings  = np.vstack(index_embeddings)
+    search_embeddings = np.vstack(search_embeddings)
 
     ## Create index
     index = create_faiss_index(index_embeddings)
@@ -154,20 +200,21 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
 
     #FILENAME = '../data/corrupted_addresses_dedup.feather'
-    FILENAME = '../data/corrupted_companies_dedup.feather'
-    data = pd.read_feather(FILENAME)
+    #FILENAME = '../data/corrupted_companies_dedup.feather'
+    #data = pd.read_feather(FILENAME)
 
     #test_dedup(dedupe_faiss, data, 'address_true', k=5)
     #test_dedup(dedupe_faiss, data, 'company', k=5)
     #test_dedup(dedupe_knn, data, 'company', k=5)
     #test_dedup(dedupe_approx_knn, data, 'company', k=5)
-    test_dedup(dedupe_approx_knn, data, 'company', k=5)
+    #test_dedup(dedupe_approx_knn, data, 'company', k=5)
 
     #FILENAME = '../data/corrupted_addresses_dedup.feather'
-    #FILENAME = '../data/corrupted_companies_sim_search.feather'
-    #data = pd.read_feather(FILENAME)
+    FILENAME = '../data/corrupted_companies_sim_search.feather'
+    data = pd.read_feather(FILENAME)
 
     #test_sim_search_faiss(data, 'address_true', 'address_corrupted', k=5)
     #test_sim_search_faiss(data, 'company_true', 'company_corrupted', k=5)
     #test_sim_search_knn(data, 'company_true', 'company_corrupted', k=5)
     #test_sim_search_approx_knn(data, 'company_true', 'company_corrupted', k=5)
+    test_sim_search_faiss_byt5(data, 'company_true', 'company_corrupted', k=5)
--- char_bert/src/test_model.py
+++ char_bert/src/test_model.py
@@ -5,27 +5,30 @@ import random
 
 
 class HuggingFaceByt5Wrapper(nn.Module):
-    def __init__(self, device) -> None:
+    def __init__(self, device=None) -> None:
         super(HuggingFaceByt5Wrapper, self).__init__()
-        self.model = T5ForConditionalGeneration.from_pretrained("google/byt5-base")
-        self.tokenizer = AutoTokenizer.from_pretrained("google/byt5-base")
+        self.model = T5ForConditionalGeneration.from_pretrained("google/byt5-small")
+        self.tokenizer = AutoTokenizer.from_pretrained("google/byt5-small")
 
         self.device = device
+        if device is None:
+            self.device = T.device("cuda" if T.cuda.is_available() else "cpu") 
         self.to(self.device)
-
-
 
     def forward(self, X: str, attention_mask: T.tensor = None) -> T.tensor:
         X = self.tokenizer(X, return_tensors="pt").input_ids.to(self.device)
-        print(X)
         return self.model.generate(X, max_length=100)
+
+    def get_embeddings(self, X: str, attention_mask: T.tensor = None) -> T.tensor:
+        with T.no_grad():
+            X = self.tokenizer(X, return_tensors="pt").input_ids.to(self.device)
+            return self.model.get_input_embeddings()(X).mean(dim=-2).squeeze()
 
 
 
 if __name__ == "__main__":
-    model = HuggingFaceByt5Wrapper(T.device("cuda:0"))
-    string = f"The dog chases a <extra_id_0><extra_id_1><extra_id_2><extra_id_3> in the park."
+    model = HuggingFaceByt5Wrapper()
+    string = f"The dog chases a ball in the park."
 
-    output = model(string)
-    print(output)
-    print(model.tokenizer.batch_decode(output, skip_special_tokens=True))
+    output = model.get_embeddings(string)
+    print(output.shape)
